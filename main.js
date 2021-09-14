9981e01dc4df317057e8a5f41740fae510d0a65b
Mr_Crafter <80324603+MrCrafterGlitched@users.noreply.github.com>
2021-09-14 16:56:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload() {
+    
+}
+function setup() {
+    canvas=createCanvas(500,300)
+    canvas.center()
+    video=createCapture(VIDEO)
+    video.hide()
+}
+function draw() {
+image(video,0,0,500,300)
+}
