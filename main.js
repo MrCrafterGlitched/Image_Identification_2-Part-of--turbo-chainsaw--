14bf2b6ae002ce89731a7bb8f1ddd41c6eba7cd5
Mr_Crafter <80324603+MrCrafterGlitched@users.noreply.github.com>
2021-09-15 16:51:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,7 +6,21 @@ function setup() {
     canvas.center()
     video=createCapture(VIDEO)
     video.hide()
+    classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/ODRFbYBBb/model.json",model_loaded)
+}
+function model_loaded() {
+    console.log("Model has been loaded")
 }
 function draw() {
 image(video,0,0,500,300)
+classifier.classify(video,getResult)
+}
+function getResult(error,results) {
+    if (error) {
+        console.log(error)
+    }else{
+        console.log(results)
+        document.getElementById("Object_name").innerHTML=results[0].label
+        document.getElementById("Object_Accuracy").innerHTML=results[0].confidence.toFixed(3)
+    }
 }
